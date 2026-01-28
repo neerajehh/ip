660c1029909088b49742cefbb059bc357ac35b4b
Neeraj <neerajjrsgp@gmail.com>
2026-01-28 21:07:25 +0800

    Level-0: Rename, Greet, Exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# TASKbOT User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Neeraj.java
@@ -0,0 +1,11 @@
+public class Neeraj {
+    public static void main(String[] args) {
+        String logo = "===========================\n"
+                + "        NEERAJ BOT        \n"
+                + "===========================\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Neeraj");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
