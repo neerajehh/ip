81ce60a2d1e6fcf6d465504dedd5bc88a76155d2
Neeraj <neerajjrsgp@gmail.com>
2026-01-30 13:26:38 +0800

    Level-1: Echo

--- src/main/java/Neeraj.java
+++ src/main/java/Neeraj.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Neeraj {
     public static void main(String[] args) {
         String logo = "===========================\n"
@@ -6,6 +8,22 @@ public class Neeraj {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Neeraj");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("===========================\n");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println("===========================");
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("===========================");
+                break;
+            } else {
+                System.out.println(input);
+                System.out.println("===========================\n");
+            }
+        }
     }
 }
